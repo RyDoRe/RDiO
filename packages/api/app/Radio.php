4e9b61a890c12017f3f48468b4795c25f3dea938
Jonas Lieber <jns.lieber@gmail.com>
2020-06-06 14:40:44 +0200

    added backend functionality for favorites

--- packages/api/app/Radio.php
+++ packages/api/app/Radio.php
@@ -15,4 +15,14 @@ class Radio extends Model
     {
         return $this->belongsTo('App\Playlist');
     }
+
+    /**
+     * The favorites that belong to the radio.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany<\App\User>
+     */
+    public function favorites()
+    {
+        return $this->belongsToMany('App\User', 'favorites', 'radio_id', 'user_id')->withTimestamps();
+    }
 }
