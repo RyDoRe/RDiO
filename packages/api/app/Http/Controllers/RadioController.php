0cbd34366f86d9d9ce4e381de13ac63eb928564f
Jonas Lieber <jns.lieber@gmail.com>
2020-06-07 10:41:52 +0200

    Reduced throttling

--- packages/api/app/Http/Controllers/RadioController.php
+++ packages/api/app/Http/Controllers/RadioController.php
@@ -7,6 +7,9 @@ use Validator;
 use Illuminate\Http\Request;
 use App\Exceptions\Handler;
 use Illuminate\Support\Facades\File;
+use Symfony\Component\HttpFoundation\StreamedResponse;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\PhpExecutableFinder;
 
 class RadioController extends Controller
 {
@@ -86,88 +89,103 @@ class RadioController extends Controller
     {
         $radio = Radio::find($id);
 
+        // Check if radio is activated
         if ($radio->active === 0) {
             return;
         }
 
-        $settings = [
-            'buffer_size' => 16384,
-            'max_listen_time' => 14400,
-        ];
-
         $songs = $radio->playlist->songs;
+
+        // Stream settings
+        $settings = [
+          'buffer_size' => 16384,
+          'max_listen_time' => 14400,
+        ];
 
         $playfiles = [];
 
+        // Load song metadata
         foreach ($songs as $song) {
+          if (file_exists($song->path)) {
             $filesize = filesize($song->path);
 
-
             $playfile = [
-                'filepath' => $song->path,
-                'filesize' => $filesize,
-                'playtime' => ($filesize * 8) / (128 * 1024),
-                'audiostart' => 45,
-                'audioend' => $filesize,
-                'audiolength' => $filesize - 45,
+              'filepath' => $song->path,
+              'filesize' => $filesize,
+              'playtime' => ($filesize * 8) / (128 * 1024),
+              'audiostart' => 45,
+              'audioend' => $filesize,
+              'audiolength' => $filesize - 45,
             ];
 
             $playfiles[] = $playfile;
+          }
         }
 
         $start_time = microtime(true);
 
+        // Calc total playtime
         $total_playtime = 0;
         foreach ($playfiles as $playfile) {
-            $total_playtime += $playfile['playtime'];
+          $total_playtime += $playfile['playtime'];
         }
 
+        // Calc playtime based on play position
         $play_sum = 0;
         $play_pos = $start_time % $total_playtime;
         $i = -1;
         foreach ($playfiles as $i => $playfile) {
-            $play_sum += $playfile['playtime'];
-            if ($play_sum > $play_pos) {
-                break;
-            }
+          $play_sum += $playfile['playtime'];
+          if ($play_sum > $play_pos) {
+            break;
+          }
         }
 
         if ($i === -1) {
-            return;
+          return;
         }
 
+        // Calc track position
         $track_pos = ($playfiles[$i]['playtime'] - $play_sum + $play_pos)
-            * $playfiles[$i]['audiolength'] / $playfiles[$i]['playtime'];
+          * $playfiles[$i]['audiolength'] / $playfiles[$i]['playtime'];
 
-        header('Content-Type: audio/mpeg');
-
-        /* $o = $i; */
+        // Load buffer based on track position
         // @phpstan-ignore-next-line
         $old_buffer = substr(
-            file_get_contents(
-                $playfiles[$i]['filepath']
-            ),
-            $playfiles[$i]['audiostart'] + $track_pos,
-            $playfiles[$i]['audiolength'] - $track_pos
+          file_get_contents(
+            $playfiles[$i]['filepath']
+          ),
+          $playfiles[$i]['audiostart'] + $track_pos,
+          $playfiles[$i]['audiolength'] - $track_pos
         );
 
-        while (time() - $start_time < $settings['max_listen_time']) {
+        // Return buffer in Streamed Response
+        return response()->stream(function () use ($start_time, $settings, $i, $playfiles, $old_buffer) {
+          while (time() - $start_time < $settings['max_listen_time']) {
+            // Load next song
             $i = ++$i % count($playfiles);
             // @phpstan-ignore-next-line
             $buffer = $old_buffer.substr(
-                file_get_contents($playfiles[$i]['filepath']),
-                $playfiles[$i]['audiostart'],
-                $playfiles[$i]['audiolength']
+              file_get_contents($playfiles[$i]['filepath']),
+              $playfiles[$i]['audiostart'],
+              $playfiles[$i]['audiolength']
             );
 
+            // Split the buffer into small parts
             for ($j = 0; $j < floor(strlen($buffer) / $settings['buffer_size']); $j++) {
-                echo substr($buffer, $j * $settings['buffer_size'], $settings['buffer_size']);
-                usleep(500000);
+              echo substr($buffer, $j * $settings['buffer_size'], $settings['buffer_size']);
+              ob_flush();
+              flush();
             }
+            // Throttle the connection to minimize
+            // the bandwidth. The value depends
+            // more or less on the server resources.
+            sleep(2);
 
-            /* $o = $i; */
+            // Load next part of the buffer
             $old_buffer = substr($buffer, $j * $settings['buffer_size']);
-        }
+          }
+        }, 200, ['Cache-Control' => 'no-cache', 'Content-Type' => 'audio/mpeg']);
     }
 
     /**
@@ -201,6 +219,7 @@ class RadioController extends Controller
 
         $favorite = $request->auth->favorites()->find($request->input('radio_id'));
 
+        // Toggle favorites by deleting existing one or attach a new one
         if (empty($favorite)) {
             // @phpstan-ignore-next-line
             $radio->favorites()->attach($request->auth->id);
